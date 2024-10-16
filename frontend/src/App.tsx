30115637895f7ba3d0c8ed96c44baa5716119c2b
Bibek-only <bibekbibek966@gmail.com>
2024-10-16 07:44:03 +0530

    start the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-red-200 text-black text-xl">
+      Hello! welcome to my project
+    </div>
+  )
+}
+
+export default App
