7c3ffa7c1d98d48bca0f66298efe2d45bac3e52d
Bibek-only <bibekbibek966@gmail.com>
2024-10-26 20:19:40 +0530

    designed the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,11 +1,58 @@
-
+import { FaTwitter, FaFacebook, FaInstagram, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-red-400">
-      hello i am footer
-    </div>
-  )
-}
+    <section className=" w-full py-16 max-lg:py-8 border-t border-gray-300">
+      <div className="top mb-28 max-lg:mb-6  lg:w-11/12 m-auto flex justify-evenly text-gray-300 max-lg:flex-wrap gap-2">
+        <ul className="flex flex-col gap-6">
+          <li className="text-2xl font-bold mb-6  text-indigo-500 hover:text-indigo-600 duration-300">Bibe-blog</li>
+          <li>about</li>
+          <li>features</li>
+          <li>career</li>
+        </ul>
 
-export default Footer
+        <ul className="flex flex-col gap-6">
+          <li className="text-xl font-bold mb-6 text-white ">Home</li>
+          <li>Services</li>
+          <li>Portfolio</li>
+          <li>Contact Us</li>
+        </ul>
+
+        <ul className="flex flex-col gap-6">
+          <li className="text-xl font-bold mb-6 text-white ">Blog</li>
+          <li>Guides</li>
+          <li>Resources</li>
+          <li>Support</li>
+        </ul>
+        <ul className="flex flex-col gap-6">
+          <li className="text-xl font-bold mb-6 text-white ">Dashboard</li>
+          <li>Profile</li>
+          <li>Settings</li>
+          <li>Logout</li>
+        </ul>
+      </div>
+      <div className="lower flex flex-wrap justify-evenly items-center lg:w-11/12 buttom m-auto">
+        <ul className="flex max-lg:gap-2 max-lg:flex-wrap items-center gap-8 font-thin text-white">
+          <li>
+            <FaTwitter />
+          </li>
+          <li>
+            <FaFacebook />
+          </li>
+          <li>
+            <FaInstagram /> 
+          </li>
+          <li>
+            <FaGithub /> 
+          </li>
+          <li>Privacy Policy</li>
+          <li>Terms & Conditions</li>
+          <i>Support</i>
+        </ul>
+        <p className="font-thin text-gray-300">© Copyright 2021, All Rights Reserved by Postcraft</p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
